e812bdf69e2e4a3eb989049055e70ac5b935c67e
Santiago Jaramillo <santiagojaramillo2611@gmail.com>
2025-06-10 12:52:46 -0500

    Crear clase Principal con mapa de clasificación y lista de animales

---
+++ ClasificadorAnimales.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ClasificadorAnimales {
+    private static Map<String, List<Animal>> clasificacion = new TreeMap<>();
+    private static List<Animal> animales = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
